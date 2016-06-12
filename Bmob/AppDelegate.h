a0e5aa0dee651c074fd7e99c2b160474ea6acb77
FJHang <1734917@qq.com>
2016-06-12 23:01:28 +0800

    Bmob create

---
+++ Bmob/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bmob
+//
+//  Created by 冯军航 on 16/6/12.
+//  Copyright © 2016年 冯军航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bmob/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bmob
+//
+//  Created by 冯军航 on 16/6/12.
+//  Copyright © 2016年 冯军航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
